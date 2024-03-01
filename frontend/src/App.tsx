2c8705cb41efb2a4cc6616b32935a51878d082d6
azatbek <sadykowazatbek@gmail.com>
2024-03-01 20:49:50 +0600

    Добавлены UI компоненты, как appToolbar, anonymousMenu для анонимных пользователей, и userMenu для зарегистрированных пользователей. Добавлены компоненты login для восстановление пользователя, register для регистрации пользователя. AxiosApi для связи запросов с сервера.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import AppToolbar from './components/UI/AppToolbar.tsx';
+import Login from './features/Users/Login.tsx';
+import Register from './features/Users/Register.tsx';
 
 function App() {
   return (
     <>
-
+      <header>
+        <AppToolbar />
+      </header>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   )
 }
